864211174d43eba34d5d9812c4b9c80de54b7a25
Jonathan Amsterdam <jba@google.com>
2021-02-08 07:22:53 -0500

    skip test redone entirely with build tags

--- _skiptest/decode.go
+++ _skiptest/decode.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//+build version2
+// +build version2,decode
 
 // This can't be a normal XXX_test.go file, because the test binary's package
 // name (as in reflect.TypeOf, etc.) is not "main", but something constructed
@@ -23,30 +23,31 @@ import (
 func main() {
 	f, err := os.Open("skip.enc")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("FAIL: %v", err)
 	}
 	d := codec.NewDecoder(f, nil)
 
 	var got Skip
 	if err := d.Decode(&got); err != nil {
-		log.Fatal(err)
+		log.Fatalf("FAIL: %v", err)
 	}
+	fmt.Println("decoded skip.enc")
 	var want Skip
 	if !cmp.Equal(got, want) {
-		log.Fatalf("got %+v, want %+v", got, want)
+		log.Fatalf("FAIL: got %+v, want %+v", got, want)
 	}
 	f.Close()
 
 	f, err = os.Open("skip.enc")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("FAIL: %v", err)
 	}
 	defer f.Close()
 	d = codec.NewDecoder(f, &codec.DecodeOptions{DisallowUnknownFields: true})
 	if err := d.Decode(&got); err == nil {
-		log.Fatal("got nil, want error")
+		log.Fatal("FAIL: got nil, want error")
 	} else if !strings.Contains(err.Error(), "unknown field") {
-		log.Fatalf("Decode error is unexpected: %q", err)
+		log.Fatalf("FAIL: Decode error is unexpected: %q", err)
 	}
-	fmt.Println("decode test passed")
+	fmt.Println("PASS")
 }
--- _skiptest/run.sh
+++ _skiptest/run.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
-go run -tags version1 gen.go skip1.go
+go run -tags version1,gen .
+go run -tags version1,encode .
 
-go run -tags version1 encode.go skip1.go skip.gen.go
+rm skip.gen.go
 
-go run -tags version2 gen.go skip2.go
-
-go run -tags version2 decode.go skip2.go skip.gen.go
+go run -tags version2,gen .
+go run -tags version2,decode .
 
 rm skip.gen.go
 rm skip.enc
